c5c2ad33a418181a85ae07c68571a52a1c49e84a
SemenOtvesov <99428817+SemenOtvesov@users.noreply.github.com>
2023-07-10 21:41:17 +0300

    Delete webpack projects/trainingDiary directory

--- webpack projects/trainingDiary/maket/js/types/functionTypes.ts
+++
@@ -1,6 +0,0 @@
-export type setScrollWidthType = (a: number)=> void
-
-export interface layoutInterface {
-    scrollWidth: number,
-    activeTraining: undefined | string
-}
--- webpack projects/trainingDiary/maket/js/types/mainFunctionsTypes.ts
+++
@@ -1,6 +0,0 @@
-export type TgetUrlParams = {
-    el: HTMLElement, 
-    pathWebp: string, 
-    pathPng: string,
-    previewIterator: number
-}
