c9464ef77c4c193f4572a323cded0d11a200220a
SemenOtvesov <99428817+SemenOtvesov@users.noreply.github.com>
2023-10-24 23:02:15 +0300

    Delete webpack projects/BikePark/maket/js/mainFunctions directory

--- webpack projects/BikePark/maket/js/mainFunctions/events/window/outsideSelectClick/outsideSelectClick.ts
+++
@@ -1,9 +0,0 @@
-import { ReactEventHandler } from 'react';
-
-const outsideSelectItem: ReactEventHandler = (event) => {
-    const target = event.target as HTMLElement
-    if(!target.closest('#select')){
-        document.querySelectorAll('#select').forEach(el=>el.classList.remove('active'))
-    }
-}
-export default outsideSelectItem
