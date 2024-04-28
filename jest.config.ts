a1f34a3241d89ec5291269971df0e971214e2a2c
SemenOtvesov <smen.otvrsov@mail.ru>
2024-04-29 02:44:05 +0300

    deleteOldProject

--- jest.config.ts
+++
@@ -1,8 +0,0 @@
-import { defaults } from 'jest-config';
-
-const config = {
-    moduleFileExtensions: [...defaults.moduleFileExtensions, 'mts'],
-    transform: {'^.+\\.tsx?$': 'ts-jest'}
-};
-
-export default config;
